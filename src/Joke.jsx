8863a24e83e6a23eda04b97ed4637626cc07cab9
Kunj Tyagi <kunjtyagi24@gmail.com>
2024-11-15 16:40:26 +0530

    Comment

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,25 +1,32 @@
 import { useState, useEffect } from "react";
 
+// Define the Joker component
 export default function Joker() {
+  // Initialize state to hold the joke
   let [joke, setJoke] = useState({});
   const URL = "https://official-joke-api.appspot.com/random_joke";
 
+  // Function to fetch a new joke
   const getNewJoke = async () => {
     let response = await fetch(URL);
     let jsonResponse = await response.json();
+    // Update the state with the new joke
     setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
   };
 
+  // useEffect hook to fetch the first joke when the component mounts
   useEffect(() => {
     async function getFirstJoke() {
       let response = await fetch(URL);
       let jsonResponse = await response.json();
       console.log(jsonResponse);
+      // Update the state with the first joke
       setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
     }
     getFirstJoke();
   }, []);
 
+  // Render the component
   return (
     <div>
       <h3>Joker!</h3>
